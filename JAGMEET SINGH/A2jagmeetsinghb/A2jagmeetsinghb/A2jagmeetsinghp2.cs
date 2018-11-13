550261f2066c9f16dc01de4d70407353fc82094e
singhjagmeet <45006024+singhjagmeet@users.noreply.github.com>
2018-11-14 00:15:45 +0530

    Add files via upload

---
+++ JAGMEET SINGH/A2jagmeetsinghb/A2jagmeetsinghb/A2jagmeetsinghp2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2jagmeetsinghb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, d;
+            Console.WriteLine("student's age");
+            string ag = Console.ReadLine();
+            double age = double.Parse(ag);
+            Console.WriteLine("international student(yes,no)");
+            string a = Console.ReadLine();
+            Console.WriteLine("resgistration semester(fall,winter,summer)");
+            string b = Console.ReadLine();
+
+            if(a=="no")
+            {
+                if(age<=18)
+                {
+                    c = 300 + (0.13 * 300);
+                    Console.WriteLine("base tuition $" + c);
+                }
+                else if (age <=49)
+                {
+                    c = 300 + (0.13 * 300);
+                    Console.WriteLine("base tuition $" + c);
+                }
+                else
+                {
+                    c = 400 + (0.13 + 400);
+                    Console.WriteLine("base tuition $" + c);
+                }
+            }
+            else if(a=="yes")
+            {
+                if(age<=18)
+                {
+                    c = 400 + (0.13 + 400);
+                    Console.WriteLine("international student fee" + c);
+                }
+                if (age <= 49)
+                {
+                    c = 600 + (0.13 + 600);
+                    Console.WriteLine("international student fee" + c);
+                }
+                else
+                {
+                    c = 500 + (0.13 + 500);
+                    Console.WriteLine("international student fee" + c);
+                }
+            }
+            switch(b)
+            {
+                case "fall":
+                    {
+                        d = 250 + (0.13 + 250);
+                        Console.WriteLine("registration fee for semester $" + d);
+                        double tax = 250 * 0.13;
+                        Console.WriteLine("hst $" + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total:" + total);
+                        break;
+                    }
+                case "winter":
+                    {
+                        d = 220 + (0.13 + 220);
+                        Console.WriteLine("registration fee for semester $" + d);
+                        double tax = 220 * 0.13;
+                        Console.WriteLine("hst $" + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total:" + total);
+                        break;
+                    }
+                case "summer":
+                    {
+                        d = 150 + (0.13 + 150);
+                        Console.WriteLine("registration fee for semester $" + d);
+                        double tax = 150 * 0.13;
+                        Console.WriteLine("hst $" + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total:" + total);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("enter volid month");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ JAGMEET SINGH/A2jagmeetsinghp1/A2jagmeetsinghp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2jagmeetsinghp1
+{
+    class Program
+    {
+        static double sphere(double r, double p)
+        {
+            double sphere;
+            sphere = 4 / 3 * p * r * r * r;
+            return sphere;
+        }
+        static double sphere(double j, double h, double p)
+        {
+            double sphere;
+            sphere = p * j * j * h;
+            return sphere;
+        }
+        static int sphere(int l, int b, int h)
+        {
+            int sphere;
+            sphere = l * b * h;
+            return sphere;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1:sphere");
+            Console.WriteLine("2:cylinder");
+            Console.WriteLine("3:rectanguler prism");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+            if (y == 1)
+            {
+                Console.WriteLine("enter the value of radius");
+                string u = Console.ReadLine();
+                double r = double.Parse(u);
+                double result;
+                result = sphere(3.14, r);
+                Console.WriteLine("volume of sphere =" + result);
+            }
+            else if (y == 2)
+            {
+                Console.WriteLine("enter the value of radius");
+                string i = Console.ReadLine();
+                double j = double.Parse(i);
+                Console.WriteLine("enter the value of height");
+                string k = Console.ReadLine();
+                double h = double.Parse(i);
+                double res;
+                res = sphere(3.14, j, h);
+                Console.WriteLine("volume of cylinder = " + res);
+            }
+            else if (y ==3)
+            {
+                Console.WriteLine("enter the length of rectangle");
+                string a = Console.ReadLine();
+                int l = int.Parse(a);
+                Console.WriteLine("enter the breadth of rectangle");
+                string c = Console.ReadLine();
+                int b = int.Parse(c);
+                Console.WriteLine("enter the height of rectangle");
+                string d = Console.ReadLine();
+                int h = int.Parse(d);
+                int resu;
+                resu = sphere(l, b, h);
+                Console.WriteLine("volume of rectangle prism =" + resu);
+            }
+        } 
+    }
+}
